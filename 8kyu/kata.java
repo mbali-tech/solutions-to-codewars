95e51e9c6fc0fb7dbea922387651248dd40bbae9
Mbali <52870123+mbali-tech@users.noreply.github.com>
2024-01-11 23:24:37 +0200

    min-max kata exercise

---
+++ 8kyu/kata.java
@@ -0,0 +1,31 @@
+/*Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) 
+that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+
+Examples (Input -> Output)
+* [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
+* [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
+* [42, 54, 65, 87, 0]             -> min = 0, max = 87
+* [5]                             -> min = 5, max =5 */
+
+public class Kata {
+
+  public int min(int[] list) {
+    int min = list[0];
+    for (int number : list) {
+      if (number < min) {
+        min = number;
+      }
+    }
+    return min;
+  }
+  
+  public int max(int[] list) {
+    int max = list[0];
+    for (int number : list) {
+      if (number > max) {
+        max = number;
+      }
+    }
+    return max;
+  }
+}
